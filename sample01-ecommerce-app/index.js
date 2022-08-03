ba97330daedff703204a70d9a02d9e0888257f05
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-03 21:06:13 +0900

    feat(sample01): add products type and sample data

--- sample01-ecommerce-app/index.js
+++ sample01-ecommerce-app/index.js
@@ -1,15 +1,28 @@
 const { ApolloServer, gql } = require("apollo-server");
+const products = require("./products");
 
 const typeDefs = gql`
   type Query {
     hello: String
+    products: [Product!]!
+  }
+
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => {
-      return "world!!";
+      return "world!";
+    },
+    products: () => {
+      return products;
     },
   },
 };
