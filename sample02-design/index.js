20facd3bfaea5b29765bc4a1645a50fc7fce072c
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-06 23:07:58 +0900

    feat(sample02): add sample design project

---
+++ sample02-design/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    cars: [Car!]!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      cars: () => [{ id: 1, color: "blue", make: "Toyota" }],
+    },
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
