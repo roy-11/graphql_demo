14fd790a1d8305443de4a158992ba62cadfcd58c
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-17 22:17:48 +0900

    feat(sample03): add me query

--- sample03-socialmedia-app/src/resolvers/Query.ts
+++ sample03-socialmedia-app/src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from "../index";
 
 export const Query = {
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
   posts: (_: any, __: any, { prisma }: Context) => {
     return prisma.post.findMany({
       orderBy: [
