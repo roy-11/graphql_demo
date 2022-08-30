78d490f38561a45237afd6b9b0c582b9a97f5657
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-30 15:46:01 +0900

    feat(sample04): task 4-6

--- sample04-prisma-workshop/script.ts
+++ sample04-prisma-workshop/script.ts
@@ -25,6 +25,29 @@ async function main() {
   //   },
   // });
   // console.log(result3);
+
+  // Task5
+  // const result5 = await prisma.post.create({
+  //   data: {
+  //     title: "Hello World",
+  //   },
+  // });
+  // console.log(result5);
+
+  // Task6
+  // const result6 = await prisma.post.update({
+  //   where: {
+  //     id: 4,
+  //   },
+  //   data: {
+  //     author: {
+  //       connect: {
+  //         id: 1,
+  //       },
+  //     },
+  //   },
+  // });
+  // console.log(result6);
 }
 
 main()
