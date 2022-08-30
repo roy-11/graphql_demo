041e60c8da040d1f42220a08a129f1d48fc753a1
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-30 15:23:15 +0900

    feat(sample04): task 1-3

--- sample04-prisma-workshop/script.ts
+++ sample04-prisma-workshop/script.ts
@@ -1,16 +1,36 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-// A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  // Task1
+  const result1 = await prisma.user.findMany();
+  console.log(result1);
+
+  // Task2
+  // const result2 = await prisma.user.create({
+  //   data: {
+  //     email: "testemail6@test.test.test",
+  //   },
+  // });
+  // console.log(result);
+
+  // Task3
+  // const result3 = await prisma.user.update({
+  //   where: {
+  //     email: "testemail6@test.test.test",
+  //   },
+  //   data: {
+  //     name: "Alice",
+  //   },
+  // });
+  // console.log(result3);
 }
 
 main()
-  .catch(e => {
-    throw e
+  .catch((e) => {
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
